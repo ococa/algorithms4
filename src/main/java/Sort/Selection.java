90d816798934b8345cf7e63b044728ad19531e0b
ococa-com <wangchao.personal@gmail.com>
2019-07-03 16:32:26 +0800

    feat: init & selection

---
+++ src/main/java/Sort/Selection.java
@@ -0,0 +1,32 @@
+package Sort;
+
+import com.sun.tools.javac.util.Assert;
+
+/**
+ * @program: algsW
+ * @description: 选择排序
+ * @author: Mr.Wang
+ * @create: 2019-07-03 16:04
+ **/
+
+public class Selection {
+    public static void main(String[] args) {
+        Comparable[] test = {3,4,5,6,8,2};
+        sort(test);
+        SortTemplate.show(test);
+    }
+
+    public static void sort(Comparable[] array) {
+        int length = array.length;
+        for (int i = 0; i < length; i++) {
+            // 如果i+1到length-1中最小的数小于array[i];
+            // 将array[i+1] 到array[length-1]中最小的数和array[i]交换
+            for (int j = i + 1; j < length; j++) {
+                if (SortTemplate.isLess(array[j], array[i])) {
+                    SortTemplate.exchange(array, i, j);
+                }
+            }
+        }
+
+    }
+}
---
+++ src/main/java/Sort/SortTemplate.java
@@ -0,0 +1,52 @@
+package Sort;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * @program: algsW
+ * @description: 基础排序算法入口
+ * @author: Mr.Wang
+ * @create: 2019-07-03 15:52
+ **/
+
+public class SortTemplate {
+
+
+    /**
+     * 如果v<w返回 true 否则返回false
+     * @param v
+     * @param w
+     * @return
+     */
+    public static boolean isLess(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    // 交换数组中i 和j位置的元素
+    public static void exchange(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    // 打印数组
+    public static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + "");
+        }
+        StdOut.println();
+    }
+
+    // 判断数组是否已经被排序
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (isLess(a[i], a[i-1])) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = {};
+
+    }
+}
